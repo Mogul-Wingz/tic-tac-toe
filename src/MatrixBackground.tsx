e937a6709a5b125e9b0254cdbaf45c1c91dda23f
Mogul-Wingz <jaggertate38@gmail.com>
2025-02-17 19:31:44 -0700

    Update to confetti

--- src/MatrixBackground.tsx
+++ src/MatrixBackground.tsx
@@ -39,7 +39,7 @@ export default function MatrixBackground() {
   return (
     <canvas
       ref={canvasRef}
-      style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}
+      style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -2 }}
     />
   )
 }
